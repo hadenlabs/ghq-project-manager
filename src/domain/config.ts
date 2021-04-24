9a6e672d3638cab925972d699bc6c70183d15878
@slovacus <slovacus@gmail.com>
2021-04-24 13:58:51 -0500

    chore: implement code domain (#3)

---
+++ src/domain/config.ts
@@ -0,0 +1,19 @@
+import { WorkspaceConfiguration } from 'vscode';
+
+/**
+ * @typedef Config
+ * @type {Object}
+ * @class Config
+ */
+export default class Config extends Object {
+  baseProjectFolders: string;
+
+  constructor(vscodeCfg?: WorkspaceConfiguration) {
+    super();
+    this.baseProjectFolders = '';
+  }
+
+  loadConfigFromVsCode(vscodeConfig: WorkspaceConfiguration) {
+    this.baseProjectFolders = vscodeConfig.get('ghqRoot', '');
+  }
+}
