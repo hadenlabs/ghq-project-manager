9d4f44dde49ae2b8cd69a4887aa3a637021e7c75
@slovacus <slovacus@gmail.com>
2021-04-24 14:02:12 -0500

    chore: implement code manager (#3)

---
+++ src/extension.ts
@@ -0,0 +1,42 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import ProjectManager from './ghqProjectManager';
+import Config from './domain/config';
+
+const cfg = new Config(vscode.workspace.getConfiguration('ghqProjectManager'));
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+function activate(context: vscode.ExtensionContext) {
+  const projectManager = new ProjectManager(cfg, context.globalState);
+
+  const disposable = vscode.commands.registerCommand('ghqProjectManager.openProject', function () {
+    projectManager.showProjectList(false);
+  });
+
+  const newWindowdisposable = vscode.commands.registerCommand(
+    'ghqProjectManager.openProjectNewWindow',
+    function () {
+      projectManager.showProjectList(true);
+    }
+  );
+
+  context.subscriptions.push(disposable, newWindowdisposable);
+
+  context.subscriptions.push(
+    vscode.workspace.onDidChangeConfiguration(() => {
+      projectManager.config = new Config(vscode.workspace.getConfiguration('ghqProjectManager'));
+      projectManager.refreshList.bind(projectManager, true);
+    })
+  );
+}
+
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() {
+  //clear things
+}
+
+exports.deactivate = deactivate;
---
+++ src/recentItems.ts
@@ -0,0 +1,36 @@
+import { Memento } from 'vscode';
+import RecentItem from './domain/RecentItem';
+
+export default class RecentItems {
+  state: Memento;
+  listSize: number;
+  list: Array<RecentItem>;
+  /**
+   * Creates an instance of RecentItems.
+   *
+   * @param {Memento} state Global extension state.
+   * @param {number} [listSize=5] Recent items list size.
+   */
+  constructor(state: Memento, listSize = 5) {
+    this.state = state;
+    this.listSize = listSize;
+    this.list = this.state.get('recent', []);
+  }
+  addProject(projectPath: string, gitRepo: string) {
+    const idx = this.list.findIndex((p) => p.projectPath === projectPath);
+    if (idx >= 0) {
+      this.list[idx].lastUsed = new Date().getTime();
+    } else {
+      this.list.push(new RecentItem(projectPath, gitRepo, new Date().getTime()));
+    }
+
+    this.sortList();
+    this.state.update('recent', this.list);
+  }
+  sortList() {
+    this.list = this.list.sort((a, b) => b.lastUsed - a.lastUsed);
+    if (this.list.length > this.listSize) {
+      this.list = this.list.slice(0, this.listSize - 1);
+    }
+  }
+}
