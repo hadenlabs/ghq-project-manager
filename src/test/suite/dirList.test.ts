b4653ca077bbf1389c99c6c5069aecbf1d9dd47c
Luis Mayta <luis@hadenlabs.com>
2021-04-24 23:52:43 -0500

    test: implement dirlist (#3)

---
+++ src/test/suite/dirList.test.ts
@@ -0,0 +1,52 @@
+/* global describe, it */
+
+import DirList from '../../domain/dirList'
+import { expect } from 'chai'
+
+suite('dirList Tests', () => {
+  test('should add directories to list', () => {
+    const dirList = new DirList()
+    dirList.add('github.com/luismayta/repo-1')
+    dirList.add('github.com/luismayta/repo-2')
+    dirList.add('github.com/luismayta/repo-3')
+    expect(dirList.dirList.length).to.be.equal(3)
+    dirList.add('github.com/luismayta/repo-4')
+    expect(dirList.dirList.length).to.be.equal(4)
+  })
+
+  test('should not add duplicates', () => {
+    const dirList = new DirList()
+    dirList.add('github.com/luismayta/repo-1')
+    dirList.add('github.com/luismayta/repo-1')
+    expect(dirList.dirList.length).to.be.equal(1)
+    dirList.add('github.com/luismayta/repo-1')
+    expect(dirList.dirList.length).to.be.equal(1)
+  })
+
+  test('should validate existing repositories', () => {
+    const dirList = new DirList()
+    dirList.add('github.com/luismayta/repo-1')
+    expect(dirList.exists('github.com/luismayta/repo-1')).to.be.true
+  })
+
+  test("shouldn't validate not existing repositories", () => {
+    const dirList = new DirList()
+    dirList.add('github.com/luismayta/repo-1')
+    expect(dirList.exists('/home/user/path2')).to.be.false
+  })
+
+  test('should concatenate two dirlist', () => {
+    const dirList = new DirList()
+    dirList.add('github.com/luismayta/repo-1')
+    dirList.add('github.com/luismayta/repo-2')
+
+    const dirList2 = new DirList()
+    dirList2.add('github.com/luismayta/repo-3')
+    dirList2.add('github.com/luismayta/repo-4')
+    dirList2.add('github.com/luismayta/repo-5')
+
+    dirList.concat(dirList2)
+
+    expect(dirList.dirList.length).to.be.equal(5)
+  })
+})
