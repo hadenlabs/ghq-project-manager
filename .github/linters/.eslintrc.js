90bd44b68ca4df3d5514e6ce0b769f27eece468b
@slovacus <slovacus@gmail.com>
2021-04-24 23:44:08 -0500

    refactor: configuration eslint without validation semicolon (#3)

--- .github/linters/.eslintrc.js
+++ .github/linters/.eslintrc.js
@@ -9,16 +9,19 @@ module.exports = {
     sourceType: 'module' // Allows for the use of imports
   },
   extends: [
+    'plugin:prettier/recommended',
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    'prettier',
     'plugin:import/warnings'
   ],
+  plugins: ['@typescript-eslint'],
   rules: {
+    "@typescript-eslint/semicolon": 'off',
+    '@typescript-eslint/member-delimiter-style': 'off',
     '@typescript-eslint/naming-convention': 'warn',
-    '@typescript-eslint/semi': 'warn',
+    '@typescript-eslint/semi': 'off',
+    'no-throw-literal': 'warn',
     curly: 'warn',
     eqeqeq: 'warn',
-    'no-throw-literal': 'warn',
     semi: 'off'
   },
   ignorePatterns: ['**/*.d.ts']
