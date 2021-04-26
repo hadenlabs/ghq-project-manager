dbf43736a18b9136815c9863a266e8c343839d12
@slovacus <slovacus@gmail.com>
2021-04-26 16:12:30 -0500

    test: implement function extension export (#8)

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,9 +1,21 @@
 import * as assert from 'assert'
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
+import { expect } from 'chai'
 import * as vscode from 'vscode'
-// import * as myExtension from '../extension';
+import * as myExtension from '../../extension'
+
+suite('extension Tests', function () {
+  suite('#Available functions', function () {
+    test('Should export activate function', function (done) {
+      expect(typeof myExtension.activate).to.be.equals('function')
+      done()
+    })
+
+    test('Should export deactivate function', function (done) {
+      expect(typeof myExtension.deactivate).to.be.equals('function')
+      done()
+    })
+  })
+})
 
 suite('Extension Test Suite', () => {
   vscode.window.showInformationMessage('Start all tests.')
